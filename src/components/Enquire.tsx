50e805ac8da6874070237a427b8c29135639a444
Aletris Pinkroot <aletris@aletrispinkroot.com>
2022-08-31 16:02:00 +1000

    wow slay

--- src/components/Enquire.tsx
+++ src/components/Enquire.tsx
@@ -11,11 +11,23 @@ const EnquireBox: React.FC = () => {
 			nested>
 			<div className='enquirePopup'>
 				<h1>Make an Enquiry</h1>
-				<form action="submit">
-					<input type="email" name="Your Email" id="email" />
-					<input type="text" name="Business Name" id="name" />
-					<input type="tel" name="Phone Number" id="phone number" />
-					<input type="text" name="Content" id="content" />
+				<form action='submit' className='enquiryForm'>
+					<div className='enquiryInput'>
+						<label htmlFor='email'>Email</label>
+						<input type='email' name='email' id='email' />
+					</div>
+					<div className='enquiryInput'>
+						<label htmlFor='name'>Business Name</label>
+						<input type='text' name='name' id='name' />
+					</div>
+					<div className='enquiryInput'>
+						<label htmlFor='phone'>Phone Number</label>
+						<input type='tel' name='phone' id='phone' />
+					</div>
+					<div className='enquiryInput'>
+						<label htmlFor='content'>Description</label>
+						<input type='text' name='content' id='content' />
+					</div>
 				</form>
 			</div>
 		</Popup>
